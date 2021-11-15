f65a856c8e12069e97c7eddfa021ff8898146c0a
Eragon35 <antipin35@yahoo.com>
2021-11-15 08:02:12 +0300

    Added indexes & correct some comments

--- src/triggers.sql
+++ src/triggers.sql
@@ -1,8 +1,5 @@
+-- Триггер для увеличения числа инцидентов у курильщика при добавлении наказания
 CREATE TRIGGER addAccident
 AFTER INSERT ON punishment
 FOR EACH ROW
 EXECUTE PROCEDURE increaseaccidents(smokerid);
-
-CREATE TRIGGER updateAccidents AFTER INSERT ON punishment
-
-UPDATE smoker SET numberofaccidents = numberofaccidents + 1 where id = 1
